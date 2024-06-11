ef6b98237537907e8ee281ad0eda74104ae74bbd
Justin Leissius <90900356+2ua@users.noreply.github.com>
2024-06-11 15:34:02 +0200

    Add files via upload

---
+++ rush00/rush04.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dconstan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 12:00:06 by dconstan          #+#    #+#             */
+/*   Updated: 2024/06/09 16:47:01 by dconstan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	mid(int x, int y)
+{
+	int	i;
+	int	a;
+
+	if (y > 1)
+	{
+		a = 0;
+		while (a < (y - 2))
+		{
+			ft_putchar('\n');
+			ft_putchar('B');
+			i = 0;
+			while (i < (x - 2))
+			{
+				ft_putchar(' ');
+				++i;
+			}
+			if (x > 1)
+				ft_putchar('B');
+			++a;
+		}
+	}
+}
+
+void	end(int x, int y)
+{
+	int	a;
+
+	if (x > 0 && y > 1)
+	{
+		ft_putchar('\n');
+		ft_putchar('C');
+		a = 0;
+		while (a < (x - 2))
+		{
+			ft_putchar('B');
+			++a;
+		}
+		if (x > 1)
+			ft_putchar('A');
+	}
+	if (x > 0 && y > 0)
+		ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	a;
+
+	if (x > 0 && y > 0)
+		ft_putchar('A');
+	a = 0;
+	while (a < (x - 2) && y > 0)
+	{
+		ft_putchar('B');
+		++a;
+	}
+	if (x > 1 && y > 0)
+		ft_putchar('C');
+	mid(x, y);
+	end(x, y);
+}
